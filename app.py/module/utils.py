aae684665b0df6acebd4d44b947827adc03db501
kesore-mn <kingkesore@gmail.com>
2024-10-30 18:12:10 +0530

    Update utils.py

--- app.py/module/utils.py
+++ app.py/module/utils.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
-from moviepy.editor import VideoFileClip
+
 def my_function():
     root = tk.Tk()
     root.withdraw()  
@@ -9,3 +9,5 @@ def my_function():
     video_path = filedialog.askopenfilename(title="Select video file", filetypes=[("Video files", "*.mp4 *.avi *.mkv *.mov")])
     root.destroy()
     return video_path
+
+my_function()

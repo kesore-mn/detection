e376f5cd4990d0abb5dbb629a718b6e19020d0f8
kesore-mn <kingkesore@gmail.com>
2024-10-30 18:15:35 +0530

    Update vid2aud.py

--- app.py/vid2aud.py
+++ app.py/vid2aud.py
@@ -1,23 +1,23 @@
-import tkinter as tk
-from tkinter import filedialog
+from module.utils import my_function
 from moviepy.editor import VideoFileClip
 
+def extract_audio():
+    video_path = my_function()  # Get the video path from the utility function
+    if not video_path:
+        print("No video file selected.")
+        return  # Exit if no video path is returned
 
-root = tk.Tk()
-root.withdraw() 
-
-
-video_path = filedialog.askopenfilename(title="Select video file", filetypes=[("Video files", "*.mp4 *.avi *.mkv *.mov")])
-
-if video_path:
     try:
-        # Load the video file
-        video = VideoFileClip(video_path)
-
-        # Extract and save the audio
-        audio = video.audio.write_audiofile("audio.mp3")
-        print("Audio has been successfully extracted and saved as 'audio.mp3'.")
+        video = VideoFileClip(video_path)  # Load the video file
+        audio_path = "extracted_audio.wav"  # Specify the output audio file name
+        file=video.audio  # Extract and save the audio
+        print(f"Audio has been successfully extracted and saved as '{audio_path}'.")
+        return file
     except Exception as e:
-        print(f"An error occurred: {e}")
-else:
-    print("No file selected.")
+        print(f"An error occurred while extracting audio: {e}")
+
+# Call the function to extract audio
+extract_audio()
+
+
+
